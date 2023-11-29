c42654d3a670d4f8eb5c39e32b77f17b8940e91d
Rashadatjou <rashadatjou.mehdi@gmail.com>
2023-11-29 22:27:50 +0100

    Initial Commit

---
+++ OpenSesame/OpenSesameApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSesameApp.swift
+//  OpenSesame
+//
+//  Created by Wolf on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenSesameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
