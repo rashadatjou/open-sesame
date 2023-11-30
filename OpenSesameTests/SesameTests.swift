c9f86c0badf2383846b3afd88c327b4df7cfd38a
Rashadatjou <rashadatjou.mehdi@gmail.com>
2023-11-30 13:29:37 +0100

    feat: Sesame package + Test Module

---
+++ OpenSesameTests/SesameTests.swift
@@ -0,0 +1,11 @@
+// SesameTests.swift
+// OpenSesame by @rashadatjou
+// macOS(13.6) with Swift(5.0)
+// 30/11/2023
+
+@testable import Sesame
+import XCTest
+
+final class SesameTests: XCTestCase {
+  func testExample() throws {}
+}
---
+++ Sesame/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+  name: "Sesame",
+  products: [
+    .library(
+      name: "Sesame",
+      targets: ["Sesame"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "Sesame"
+    ),
+  ]
+)
