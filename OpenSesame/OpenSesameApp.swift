707ccab29b3bf8338203c5678b1a230c73748bd3
Rashadatjou <rashadatjou.mehdi@gmail.com>
2023-12-17 19:30:32 +0100

    feat: app icon

--- OpenSesame/OpenSesameApp.swift
+++ OpenSesame/OpenSesameApp.swift
@@ -11,7 +11,7 @@ struct OpenSesameApp: App {
   var body: some Scene {
     MenuBarExtra(
       "Open Sesame",
-      systemImage: "door.french.open"
+      systemImage: "circle.circle.fill"
     ) {
       ContentView()
         .openSettingsAccess()
